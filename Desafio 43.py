da34dd96c9cdea87d2e2a047a0de060d40b85fce
byttencourt <byttencourt@hotmail.com>
2023-03-16 19:30:44 -0300

    v1

--- Desafio 43.py
+++ Desafio 43.py
@@ -5,11 +5,11 @@ print('Seu IMC {:.2f}.'.format(imc))
 print('-=-'*10)
 if imc < 18.5:
     print('Abaixo do peso')
-elif imc >= 18.5 and (imc < 25):
+elif 18.5 <= imc < 25:
     print('Peso Ideal')
-elif imc >= 25 and (imc < 30):
+elif 25 <= imc < 30:
     print('Sobrepeso')
-elif imc >= 30 and (imc < 40):
+elif 30 <= imc < 40:
     print('Obesidade')
-elif imc >= 40:
+else:
     print('Obesidade Morbida')
--- Desafio 44.py
+++ Desafio 44.py
@@ -1,14 +1,16 @@
 valor = int(input('Valor do Produto: '))
 print('-=-'*10)
 print('Escolha a opção de Pagamento')
-op = int(input('Opção 1: À vista dinheiro ou  cheque.\nOpção 2: À vista no Cartão.\nOpção 3: Em até 2x no Cartão.\nOpção 4: 3x ou mais\nOpção: '))
+op = int(input('[1] À vista dinheiro ou  cheque.\n[2] À vista no Cartão.\n[3] Em até 2x no Cartão.\n[4] 3x ou mais\nOpção: '))
 if op == 1:
     print('Opção com 10% de desconto')
-    print('Valor a ser pago R${}'.format(valor - valor * 10/100))
+    print('Valor a ser pago R${:.2f}'.format(valor - valor * 10/100))
 elif op == 2:
     print('Opçao com 5% de desconto')
-    print('Valor a se pago R${}'.format(valor - valor * 5/100))
+    print('Valor a ser pago R${:.2f}'.format(valor - valor * 5/100))
 elif op == 3:
-    print('Opção com parcelamento em até 2x no cartão.\nValor a ser pago R${}.'.format(valor))
+    print('Opção com parcelamento em até 2x de R${:.2f}.\nValor a ser pago R${:.2f}.'.format(valor/2, valor))
 elif op == 4:
-    print('Opção de parcelamento em 3x ou mais.\nValor a ser pago R${}'.format(valor + valor * 20 / 100))
+    print('Opção de parcelamento em 3x de R${:.2f}.\nValor a ser pago R${:.2f}'.format((valor + valor * 20 / 100)/3, valor + valor * 20 / 100))
+else:
+    print('Opção errada verifique sua opção.')
